22202f0ac9fc865dc48f903ab521179ba5134837
mitra-daneshmand <74309250+mitra-daneshmand@users.noreply.github.com>
2024-04-28 07:57:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,29 +37,6 @@ def main():
 
     metadata = datasets.init_metadata(args.tissue, args.lm, args.target_comp, args.csv_dir)
 
-    # right_ind = metadata[metadata['SIDE'] == 'R'].index
-    # left_ind = metadata[metadata['SIDE'] == 'L'].index
-    # left_df = metadata.iloc[left_ind]
-    # right_df = metadata.iloc[right_ind]
-    # left_df.reset_index(inplace=True, drop=True)
-    # right_df.reset_index(inplace=True, drop=True)
-    #
-    # for i in range(len(right_df.index)):
-    #     ind = left_df[left_df['ID'] == right_df['ID'].iloc[i]].index
-    #     if len(ind)>0:
-    #             if abs(right_df[args.target_comp].iloc[i] - left_df[args.target_comp].iloc[ind[0]]) > 1:
-    #                 alaki +=1
-
-    #9150288 9135342 9648073 9575145 9841838
-
-    # print()
-    # datasets.sample_nifti(metadata.iloc[4475], type='general')
-    # datasets.sample_nifti(metadata.iloc[640], type='general')  # general
-    # datasets.sample_nifti(metadata.iloc[3451], type='general')  # general
-    # datasets.sample_nifti(metadata.iloc[2992], type='general')  # general
-    # datasets.sample_nifti(metadata.iloc[947], type='general')  # general
-    # datasets.sample_nifti(metadata.iloc[3293], type='general')  # general
-
     normalize = True
     save_dir = 'sessions/{}/'.format(args.target_comp)
 
@@ -70,7 +47,7 @@ def main():
 
     train_val_set.reset_index(inplace=True, drop=True)
 
-    for fold_num in range(4, 5):
+    for fold_num in range(5):
         train_index = pd.read_csv(os.path.join(save_dir, 'fold_{}_train.csv'.format(str(fold_num))))
         val_index = pd.read_csv(os.path.join(save_dir, 'fold_{}_val.csv'.format(str(fold_num))))
         val_set, train_set = train_val_set.iloc[val_index.values.flatten()], train_val_set.iloc[train_index.values.flatten()]
@@ -81,13 +58,11 @@ def main():
         kvs.update('prev_model', None)
 
         df = train_set.value_counts(train_set.y_stratif)
-        # print(df)
 
         print(colored('====> ', 'blue') + f'Training fold {fold_num}....')
 
         print('Preparing datasets...')
         train_dataset, val_dataset = datasets.build_datasets(train_val_set, train_set, val_set, test_set, print_stats=False, normalize=normalize)  # metadata
-        # continue
         train_loader, val_loader = datasets.build_loaders(train_dataset, val_dataset)
 
         print('Building model and trainer...')
